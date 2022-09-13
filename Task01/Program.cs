0af054d474dd22eee61876b0d749f0728e799725
GodwinOnyia <godwinonyiagodwin@gmail.com>
2022-09-14 00:36:26 +0100

    first commit

---
+++ Task01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+int currentYear = DateTime.Now.Year;
+int[] leapYears = new int[20];
+int counter = 0;
+
+while (counter < 20)
+{
+    if ((currentYear % 4 == 0 || currentYear % 400 == 0) && currentYear % 100 != 0)
+    {
+        leapYears[counter] = currentYear;
+        counter++;
+    }
+    currentYear++;
+}
+foreach (var item in leapYears)
+{
+    Console.WriteLine(item.ToString());
+}
---
+++ Task03/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Input the Word to be converted and hit the enter key on your keyboard");
+string engword = Console.ReadLine();
+string pigLatin = string.Join(" ", engword.Split().Select(word => word.Substring(1, word.Length - 1) + word[0] + "ay"));
+Console.WriteLine(pigLatin);
